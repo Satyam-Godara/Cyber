0d0655d8bf6d98afa3e1eb5cd423a4c4a6060b2f
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-09-17 10:13:22 +0530

    Add files via upload

---
+++ CaeserCipher.py
@@ -0,0 +1,20 @@
+def caesar_encrypt(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - base + shift) % 26 + base)
+        else:
+            result += char
+    return result
+
+def caesar_decrypt(text, shift):
+    return caesar_encrypt(text, -shift)
+
+# Example
+msg = "Hello World!"
+enc = caesar_encrypt(msg, 3)
+dec = caesar_decrypt(enc, 3)
+
+print("Encrypted:", enc)
+print("Decrypted:", dec)
